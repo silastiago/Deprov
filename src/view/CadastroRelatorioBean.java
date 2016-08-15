30f57113b604cb9c8c4bf8dee1a8c1d7804e6b93
Sinf02 <Sinf02>
2016-08-15 13:13:11 -0300

    Motivo da Apreensao em mais de 1 linha no relatorio

--- src/view/CadastroRelatorioBean.java
+++ src/view/CadastroRelatorioBean.java
@@ -81,10 +81,10 @@ public class CadastroRelatorioBean implements Serializable {
  
         // ExporterOutput
         /*OutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(
-                "/var/lib/tomcat8/webapps/Deprov/resources/relatorios/FirstJasperReport.pdf");*/
+                "/var/lib/tomcat8/webapps/Deprov/resources/relatorios/"+idVeiculo+".pdf");*/
         
         OutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(
-                      "/opt/tomcat/webapps/Deprov/resources/relatorios/FirstJasperReport.pdf");
+                      "/opt/tomcat/webapps/Deprov/resources/relatorios/"+idVeiculo+".pdf");
         
         // Output
         exporter.setExporterOutput(exporterOutput);
@@ -104,8 +104,8 @@ public class CadastroRelatorioBean implements Serializable {
 	    response.setHeader("Content-Type", "application/pdf");  // Define apenas o tipo de conte�do, Utilize se necess�rio ServletContext#getMimeType() para detec��o autom�tica com base em nome de arquivo. 
 	    OutputStream responseOutputStream = response.getOutputStream();
 
-	    //String PDF_URL = "http://sinf.policiacivil.rn.gov.br:8080/Deprov/resources/relatorios/FirstJasperReport.pdf";
-	    String PDF_URL = "http://snmp.info.ufrn.br:8080/Deprov/resources/relatorios/FirstJasperReport.pdf";
+	    //String PDF_URL = "http://sinf.policiacivil.rn.gov.br:8080/Deprov/resources/relatorios/"+idVeiculo+".pdf";
+	    String PDF_URL = "http://snmp.info.ufrn.br:8080/Deprov/resources/relatorios/"+idVeiculo+".pdf";
 		// L� o conte�do do PDF
 	    URL url = new URL(PDF_URL);
 	    InputStream pdfInputStream = url.openStream();
