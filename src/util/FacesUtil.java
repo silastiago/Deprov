f1ead6e1baebdd0b0e6755552afd90776060ff60
Sinf02 <Sinf02>
2016-08-29 17:02:21 -0300

    Tentando gerar relatorios

--- src/util/FacesUtil.java
+++ src/util/FacesUtil.java
@@ -1,8 +1,16 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
+
+import conexao.ConnectionFactory;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
 
 /** Esta é uma Classe utilitaria que implementa alguns metodos que servirao para ser reutilizados em algumas classes.
 *   
@@ -23,4 +31,84 @@ public class FacesUtil {
 		HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
 		return request.getAttribute(name);
 	}	
+	
+	/*public String pegarRelatorio(int tamanhoAtributos, Map<String, Object> listaObjetos){
+		String reportSrcFile = "";
+		
+		switch (tamanhoAtributos) {
+		case 1:
+			reportSrcFile = this.escolherRelatorio(listaObjetos);
+		break;
+		case 2:
+		System.out.println("Segunda-feira");
+		
+		break;
+		case 3:
+		System.out.println("Terça-feira");
+		
+		break;
+		case 4:
+		System.out.println("Quarta-feira");
+		
+		break;
+		case 5:
+		System.out.println("Quinta-feira");
+		
+		break;
+		case 6:
+		System.out.println("Sexta-feira");
+		
+		break;
+		case 7:
+		System.out.println("Sábado");
+		
+		break;
+		default:
+		System.out.println("Este não é um dia válido!");
+		
+		}
+		
+		
+		return reportSrcFile;
+	}*/
+	
+	
+	public Map<String, String> escolherRelatorio(Map<String, Object> listaObjetos){
+		Map<String, String> mapaRelatorioParametro = new HashMap<String, String>();
+		
+		String relatorio = "";
+		String parametro = "";
+		
+		for (String key : listaObjetos.keySet()) {
+            if (key.equals("placa")) {
+				relatorio = "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/"+"Placa.jrxml";
+				parametro = "placa";
+			}else if (key.equals("placaOriginal")) {
+				relatorio = "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/"+"PlacaOriginal.jrxml";
+				parametro = "placaoriginal";
+			}else if (key.equals("fabricante.fabricante")) {
+				relatorio = "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/"+"Fabricante.jrxml";
+				parametro = "codigo_fabricante";
+			}else if (key.equals("modelo.modelo")) {
+				relatorio = "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/"+"Modelo.jrxml";
+				parametro = "codigo_modelo";
+			}else if (key.equals("chassi")) {
+				relatorio = "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/"+"Chassi.jrxml";
+				parametro = "chassi";
+			}else if (key.equals("cor.cor")) {
+				relatorio = "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/"+"Cor.jrxml";
+				parametro = "codigo_cor";
+			}
+			/*System.out.println("key: " + key + " \t values: "
+                    + listaObjetos.get(key).toString().toUpperCase());*/
+            mapaRelatorioParametro.put(parametro, relatorio);   
+        }
+		
+		
+		return mapaRelatorioParametro;
+	}
+	
+	
+	
+	
 }
--- src/view/CadastroVeiculoBean.java
+++ src/view/CadastroVeiculoBean.java
@@ -1,19 +1,42 @@
 package view;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.net.URL;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.http.HttpServletResponse;
 
+import org.primefaces.event.data.FilterEvent;
 import org.primefaces.model.StreamedContent;
 
+import conexao.ConnectionFactory;
 import model.Veiculo;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.ExporterInput;
+import net.sf.jasperreports.export.OutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import repository.Veiculos;
+import util.FacesUtil;
 import util.Repositorios;
 
 @ManagedBean(name="cadastroVeiculoBean")
@@ -24,7 +47,11 @@ public class CadastroVeiculoBean implements Serializable{
 	private Veiculo veiculo = new Veiculo();
 	private List<Veiculo> listaVeiculos = new ArrayList<Veiculo>();
 	private List<Veiculo> listaVeiculosFiltrados = new ArrayList<Veiculo>();
+	private Map<String, Object> mapaFiltros = new HashMap<String, Object>();
 	private StreamedContent file;
+	private String relatorio;
+	private String parametro;
+	private String valor;
 	
 	
 	@PostConstruct
@@ -51,6 +78,123 @@ public class CadastroVeiculoBean implements Serializable{
 		veiculos.remover(veiculo);
 		this.init();
 	}
+	
+	 public void listenFilter(FilterEvent event) {
+	        // update datasource
+	        Map<String, Object> tempString = event.getFilters();
+	        System.out.println("size filter: "+ tempString.size());
+	        FacesUtil util = new FacesUtil();
+	        
+	        
+	        
+	        for (String key : tempString.keySet()) {
+	            System.out.println("key: " + key + " \t values: "
+	                    + tempString.get(key).toString().toUpperCase());
+	            
+	            valor = tempString.get(key).toString().toUpperCase();
+	            
+	        }
+	        this.setValor(valor);
+	        Map<String, String> mapaRelatorioParametro = new HashMap<String, String>();
+	        mapaRelatorioParametro = util.escolherRelatorio(tempString);
+	        
+	        for (String key : mapaRelatorioParametro.keySet()) {
+	            System.out.println("Parametro: " + key + " \t Relatorio: "
+	                    + mapaRelatorioParametro.get(key).toString());
+	            parametro = key;
+	            relatorio = mapaRelatorioParametro.get(key).toString();
+	        }
+	        this.setParametro(parametro);
+	        this.setRelatorio(relatorio);
+	    }
+	
+	//public void gerarRelatorio(ActionEvent event) throws JRException, IOException{
+	 public String gerarRelatorio() throws JRException, IOException {
+		//Veiculo veiculo = (Veiculo) event.getComponent().getAttributes().get("codigo");
+		
+		//String codigo = veiculo.getCodigo().toString();
+		//System.out.println("Codigo de veiculos: "+ veiculo.getCodigo());
+		
+		//int idVeiculo = Integer.parseInt(codigo);
+				
+		
+		 ConnectionFactory conexao = new ConnectionFactory();
+		 
+		 String reportSrcFile = this.getRelatorio();
+		 
+		//String reportSrcFile = "/var/lib/tomcat8/webapps/Deprov/resources/relatorios/RelatorioVeiculo.jrxml";
+	        // First, compile jrxml file.
+	    JasperReport jasperReport =    JasperCompileManager.compileReport(reportSrcFile);
+	 
+	    Connection conn = conexao.getConnection();
+	    
+	    // Parameters for report
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        
+        parameters.put(this.getParametro(), this.getValor());
+	    
+        
+        JasperPrint print = JasperFillManager.fillReport(jasperReport,
+                parameters, conn);
+         
+        // PDF Exportor.
+        JRPdfExporter exporter = new JRPdfExporter();
+ 
+        ExporterInput exporterInput = new SimpleExporterInput(print);
+        // ExporterInput
+        exporter.setExporterInput(exporterInput);
+	    
+     // ExporterOutput
+        /*OutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(
+                "/var/lib/tomcat8/webapps/Deprov/resources/relatorios/"+idVeiculo+".pdf");*/
+        
+        OutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(
+                      "/opt/tomcat/webapps/Deprov/resources/relatorios/parametros/1/relatorio.pdf");
+        
+        // Output
+        exporter.setExporterOutput(exporterOutput);
+        
+		 
+        SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
+        exporter.setConfiguration(configuration);
+        exporter.exportReport();	
+        System.out.print("Relatorio criado com sucesso!");
+        
+        
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+	    HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+
+	    response.reset();   // Algum filtro pode ter configurado alguns cabe�alhos no buffer de antem�o. Queremos livrar-se deles, sen�o ele pode colidir.
+	    response.setHeader("Content-Type", "application/pdf");  // Define apenas o tipo de conte�do, Utilize se necess�rio ServletContext#getMimeType() para detec��o autom�tica com base em nome de arquivo. 
+	    OutputStream responseOutputStream = response.getOutputStream();
+
+	    //String PDF_URL = "http://sinf.policiacivil.rn.gov.br:8080/Deprov/resources/relatorios/"+idVeiculo+".pdf";
+	    String PDF_URL = "http://snmp.info.ufrn.br:8080/Deprov/resources/relatorios/parametros/1/relatorio.pdf";
+		// L� o conte�do do PDF
+	    URL url = new URL(PDF_URL);
+	    InputStream pdfInputStream = url.openStream();
+
+	    // L� o conte�do do PDF e grava para sa�da
+	    byte[] bytesBuffer = new byte[2048];
+	    int bytesRead;
+	    while ((bytesRead = pdfInputStream.read(bytesBuffer)) > 0) {
+	        responseOutputStream.write(bytesBuffer, 0, bytesRead);
+	    }    
+	    responseOutputStream.flush();
+
+	    // Fecha os streams
+	    pdfInputStream.close();
+	    responseOutputStream.close();         
+	    facesContext.responseComplete();
+        
+        return "index?faces-redirect=true";
+        
+        
+        
+	}
+	
+	
 	
 	public Veiculo getVeiculo() {
 		return veiculo;
@@ -84,6 +228,18 @@ public class CadastroVeiculoBean implements Serializable{
 		this.listaVeiculosFiltrados = listaVeiculosFiltrados;
 	}
 
+	
+
+
+	public Map<String, Object> getMapaFiltros() {
+		return mapaFiltros;
+	}
+
+
+	public void setMapaFiltros(Map<String, Object> mapaFiltros) {
+		this.mapaFiltros = mapaFiltros;
+	}
+
 
 	public StreamedContent getFile() {
 		return file;
@@ -91,5 +247,38 @@ public class CadastroVeiculoBean implements Serializable{
 
 	public void setFile(StreamedContent file) {
 		this.file = file;
-	}	
+	}
+
+
+	public String getRelatorio() {
+		return relatorio;
+	}
+
+
+	public void setRelatorio(String relatorio) {
+		this.relatorio = relatorio;
+	}
+
+
+	public String getParametro() {
+		return parametro;
+	}
+
+
+	public void setParametro(String parametro) {
+		this.parametro = parametro;
+	}
+
+
+	public String getValor() {
+		return valor;
+	}
+
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	
+	
 }
